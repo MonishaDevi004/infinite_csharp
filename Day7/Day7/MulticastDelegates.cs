db3027ae6a09fd8770c33474ad8d41042ffa17e6
MonishaDevi004 <monishanandhakumar04@gmail.com>
2025-06-26 13:13:32 +0530

    Delegate Demo

---
+++ Day7/Day7/MulticastDelegates.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public delegate long SquareDelegate(int side);
+    class MulticastDelegates
+    {
+        public  static long AreaofSquare(int side)
+        {
+            return side * side;
+        }
+
+        public static long PerimeterofSquare(int side)
+        {
+            return 4 * side;
+        }
+
+        static void Main()
+        {
+
+            SquareDelegate squareDelegate = new SquareDelegate(AreaofSquare);
+
+            //Console.WriteLine($"Area of the Square:{squareDelegate(5)}");
+
+            squareDelegate += PerimeterofSquare;
+
+            // Console.WriteLine($"Perimeter of the Square{squareDelegate(5)}");
+
+            //Explicit Invokation
+
+            Console.WriteLine($"{squareDelegate.Invoke(5)}");
+
+        }
+
+    }
+}
