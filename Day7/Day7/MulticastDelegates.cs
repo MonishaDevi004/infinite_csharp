7a3898cedca0bdd9db1ee18896aba82c3ef1ee01
MonishaDevi004 <monishanandhakumar04@gmail.com>
2025-06-26 13:18:04 +0530

    delegate update

--- Day7/Day7/MulticastDelegates.cs
+++ Day7/Day7/MulticastDelegates.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Day7
 {
     public delegate long SquareDelegate(int side);
+    public delegate void UserDelegate(string message);
     class MulticastDelegates
     {
         public  static long AreaofSquare(int side)
@@ -19,20 +20,36 @@ namespace Day7
             return 4 * side;
         }
 
+
+        public static void DisplayUser(string Message)
+        {
+            Console.WriteLine($"{Message} User");
+        }
+
+        public static void DisplayAdmin(string Message)
+        {
+            Console.WriteLine($"{Message} Admin");
+        }
         static void Main()
         {
 
             SquareDelegate squareDelegate = new SquareDelegate(AreaofSquare);
 
-            //Console.WriteLine($"Area of the Square:{squareDelegate(5)}");
+            Console.WriteLine($"Area of the Square:{squareDelegate(5)}");
 
             squareDelegate += PerimeterofSquare;
 
-            // Console.WriteLine($"Perimeter of the Square{squareDelegate(5)}");
+           Console.WriteLine($"Perimeter of the Square{squareDelegate(5)}");
 
             //Explicit Invokation
 
-            Console.WriteLine($"{squareDelegate.Invoke(5)}");
+            //Console.WriteLine($"{squareDelegate.Invoke(5)}");
+
+
+            UserDelegate userDelegate = new UserDelegate(DisplayUser);
+            userDelegate += DisplayAdmin;
+
+            userDelegate.Invoke("Hello");
 
         }
 
