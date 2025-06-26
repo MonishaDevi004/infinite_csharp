4ae6285bece45796e17938804732a5516ec05e5b
MonishaDevi004 <monishanandhakumar04@gmail.com>
2025-06-26 17:09:45 +0530

    day 7 part 2

---
+++ Day7/Day7/EventDemo1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public delegate void DisplayDelegate(string uname,string password);
+    internal class Publisher
+    {
+        //synatx
+        //accessmodifier event <delegate> eventname;
+        public event DisplayDelegate DisplayEvent;
+        public void Display(string username,string password)
+        {
+            if (username == "Sai" && password == "123")
+            {
+                DisplayEvent(username,password);
+            }
+            else
+            {
+                Console.WriteLine("UserName or Password is incorrect!!!");
+            }
+        }
+    }
+
+    class Subscriber
+    {
+        //Event handler. this will be invoked by the delegate
+        public void DisplayMessage(string username,string password)
+        {
+            Console.WriteLine($"Welcome {username}");
+        }
+    }
+    class EventDemo1
+    {
+        static void Main()
+        {
+            Publisher publisher = new Publisher();
+            Subscriber subscriber = new Subscriber();
+
+            //subscribe to the event += ,unsubscribe -=
+
+            publisher.DisplayEvent += new DisplayDelegate(subscriber.DisplayMessage);
+
+            publisher.Display("Sai", "123");
+        }
+    }
+}
---
+++ Day7/Day7/GenericClass_Method.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    class Sample
+    {
+        internal void Swap<T>(T a, T b )
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"a: {a} b :{b}");
+
+          //  temp = a + b;
+
+        }
+    }
+    class GenericClass_Method
+    {
+        static void Main()
+        {
+            Sample sample = new Sample();
+            sample.Swap<int>(10, 30);
+            sample.Swap<string>("Hello", "Hi");
+        }
+    }
+}
